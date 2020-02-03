36288b670fd94a237fed7723b65e89512f9fe61e
Kendall Price <kprice147@ksu.edu>
2020-02-03 16:50:31 -0600

    Side classes start

---
+++ Data/PandeCampo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class PanDeCampo : Side
+    {
+        public override double Price => throw new NotImplementedException();
+
+        public override uint Calories => throw new NotImplementedException();
+    }
+}
