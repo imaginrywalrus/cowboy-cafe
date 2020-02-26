090b3723895f5263bb07cbc8228093b466b39cc7
Kendall Price <kprice147@users.campus>
2020-02-26 16:47:23 -0600

    Add Order, IOrderItems, and OrderTests to project

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+* Author: Kendall Price
+
+* Class name: IOrderItem.cs
+
+* Purpose: An interface to represent order items
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+
+        double Price { get; }
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,35 @@
+﻿/*
+
+* Author: Kendall Price
+
+* Class name: Order.cs
+
+* Purpose: Represents an order put into the system
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Subtotal => 0;
+
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
